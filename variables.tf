027c593bac3af55152222ed933e9df547a4e8e82
Aneesh K B <106660452+AneeshkbAwait@users.noreply.github.com>
2022-08-12 22:53:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+
+  default = "test_pjt"
+}
+
+variable "project_vpc_cidr" {
+
+  default = "172.28.0.0/16"
+}
+
+variable "project_env" {
+
+  default = "prod"
+}
+
+variable "instance_type" {
+
+  default = "t2.micro"
+}
